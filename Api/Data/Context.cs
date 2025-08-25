c456cc7184d6df00e9e2d4eb51688ba032466845
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-08-25 15:31:29 +0300

    Apply first Migration -5

---
+++ Api/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Api.Data
+{
+    public class Context :IdentityDbContext<User>
+    {
+        public Context(DbContextOptions options) : base(options) {
+        
+        }
+    }
+}
